ee2ec8a1b4722d2a45f675c8840778b02a17a7de
github_username <you@example.com>
2024-03-25 21:49:01 +0700

    first commit

---
+++ FrontEnd/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        MyOrange: "#F2721E",
+        MyBlue: "#024FA0",
+        MyPaleGrey: "#f0ecec"
+      },
+    },
+  },
+  plugins: [],
+}
