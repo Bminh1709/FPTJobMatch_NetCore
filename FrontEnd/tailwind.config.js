d5b14872b77b4326b54f092b94dd3359a1e76617
github_username <you@example.com>
2024-03-28 10:06:59 +0700

    Finish Home Page

--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -7,7 +7,14 @@ module.exports = {
         MyOrange: "#F2721E",
         MyPaleOrange: "#fef1e8",
         MyBlue: "#024FA0",
-        MyPaleGrey: "#f0ecec"
+        MyVeryPaleGrey: "#f7f7f7",
+        MyPaleGrey: "#f0ecec",
+        MyGrey: "#d6d4d4",
+        MyDarkGrey: "#8F8F8F",
+      },
+      boxShadow: {
+        'normal': '0px 0px 20px #f0ecec',
+        'orange': '8px -8px 5px #F2721E',
       },
     },
   },
